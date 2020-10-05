246686cb475c95962cc7e9783602a4e93a7a5cd1
Sergeev Nikita <nik.bonum@gmail.com>
2020-10-05 19:41:47 +0700

    Remove Greeting all, add JavaDoc in TodoListApplication

--- src/main/java/team/smartworld/academy/todolist/TodoListApplication.java
+++ src/main/java/team/smartworld/academy/todolist/TodoListApplication.java
@@ -3,9 +3,20 @@ package team.smartworld.academy.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class TodoList Application
+ *
+ * @author Sergeev Nikita
+ * @version 1.0
+ */
 @SpringBootApplication
 public class TodoListApplication {
 
+    /**
+     * Main method from start server
+     *
+     * @param args arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(TodoListApplication.class, args);
     }
--- src/main/java/team/smartworld/academy/todolist/repo/GreetingRepository.java
+++
@@ -1,8 +0,0 @@
-package team.smartworld.academy.todolist.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import team.smartworld.academy.todolist.models.Greeting;
-
-public interface GreetingRepository extends CrudRepository<Greeting, Long> {
-
-}

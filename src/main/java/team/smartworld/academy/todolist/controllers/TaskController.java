3c9d5bf951b0e437c3a647b23d6cf41c2071b27c
Sergeev Nikita <nik.bonum@gmail.com>
2020-10-08 17:13:45 +0700

    Change method newTask

--- src/main/java/team/smartworld/academy/todolist/controllers/TaskController.java
+++ src/main/java/team/smartworld/academy/todolist/controllers/TaskController.java
@@ -19,7 +19,7 @@ public class TaskController {
      * Method for deleting Task in TodoList.
      *
      * @param id is the id of the Task to delete in DB
-     * @return is 204 NO CONTENT or Error type end status
+     * @return status or error type end status
      */
     @RequestMapping(value = "/delete_task/",
             method = RequestMethod.DELETE,
@@ -36,10 +36,10 @@ public class TaskController {
      *
      * @param id is the id of the Task to mark done in DB
      *           (uri type mark_done_task/?id={number}, json body is empty)
-     * @return error status or marked done Task ID and status 204 NO CONTENT ???
+     * @return status or error type end status
      */
 
-    // TODO метод запроса PUT или PATCH? Разобраться со статусами http ответа...
+    // TODO метод запроса PUT или PATCH? Разобраться со статусами http ответов для всех  методов...
     @RequestMapping(value = "/mark_done_task/",
             method = RequestMethod.PUT,
             produces = MediaType.APPLICATION_JSON_VALUE,
@@ -65,7 +65,7 @@ public class TaskController {
      *
      * @param changeTask changed Task date
      * @param id         is the id of the Task to change in DB
-     * @return Task
+     * @return status or error type end status
      */
     // TODO найти больше инфы и доработать!!!
     @PutMapping("/change_task/{id}")
@@ -77,22 +77,26 @@ public class TaskController {
      * Method for create Task
      *
      * @param newTask new Task Date
-     * @return Task
+     * @return Task and status or error and status
      */
-    // TODO найти больше инфы и доработать!!!
-    @PostMapping("/new_task")
-    public Task newTask(@RequestBody Task newTask) {
-        return null;
+    @RequestMapping(value = "/new_task/",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> newTask(@RequestBody Task newTask) {
+        // Проверки
+        // создание и сохранение в БД
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     /**
      * Method for getting Task
      *
      * @param id is the id for search Task in DB
-     * @return Task and status 200 OK or error and status
+     * @return Task and status or error and status
      */
     @RequestMapping(value = "/get_task/",
-            method = RequestMethod.DELETE,
+            method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getTask(@RequestParam String id) {

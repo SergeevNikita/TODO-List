4f857f5501b747bec80ca3d3d2bcdf65edbb51d2
Sergeev Nikita <nik.bonum@gmail.com>
2020-10-10 21:24:19 +0700

    Update SwaggerUI

--- src/main/java/team/smartworld/academy/todolist/TodoListApplication.java
+++ src/main/java/team/smartworld/academy/todolist/TodoListApplication.java
@@ -3,6 +3,7 @@ package team.smartworld.academy.todolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -33,9 +34,17 @@ public class TodoListApplication {
      *
      * @return Docked
      */
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                .apis(RequestHandlerSelectors.basePackage("team.smartworld.academy.todolist")).build();
+//    }
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("team.smartworld.academy.todolist")).build();
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
